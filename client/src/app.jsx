968f78f5b9aebcc3ffe9cfa387f7c62ae60ad2d6
alilthumuch <alilthumuch@gmail.com>
2018-07-19 14:16:42 -0700

    add package.json

---
+++ client/src/app.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Search from './components/Search.jsx';
+import RepoList from './components/RepoList.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      repos: []
+    }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      type: "GET",
+      url: 'http://localhost:1128/repos',
+      success: (data) => {
+        this.setState({
+          repos: data,
+        })
+      },
+      error: function(data){
+        console.log("ERRRORR")
+      }
+    })
+  }
+
+
+  search (term) {
+    console.log(`${term} was searched`);
+    $.ajax({
+      type: "POST",
+      url: ' http://localhost:1128/repos',
+      data: term,
+      success: (data) => {
+        $.ajax({
+        type: "GET",
+        url: 'http://localhost:1128/repos',
+        success: (data) => {
+          this.setState({
+            repos: data,
+          })
+        },
+        error: function(data){
+          console.log("ERRRORR")
+      }
+    })
+        // this.setState({
+        //   repos: data
+        // })
+      }
+    })
+  }
+
+  render () {
+    return (<div>
+      <h1>Github Fetcher</h1>
+      <RepoList repos={this.state.repos}/>
+      <Search onSearch={this.search.bind(this)}/>
+    </div>)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
