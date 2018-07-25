ff993b12c792ab11a2e5997dfe657211aefe7678
alilthumuch <alilthumuch@gmail.com>
2018-07-25 16:40:06 -0700

    Adding ajax call for maps location

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import Search from './Search.jsx';
 import Businesshours from './Businesshours.jsx';
 import Businessinfo from './Businessinfo.jsx';
+import Api from '../../config.js';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import Geocode from "react-geocode";
 import GoogleMap from './Map.jsx';
@@ -14,13 +15,14 @@ class App extends React.Component {
     this.state = {
       businessHours: {},
       businessInfo: {},
-      address: [],
+      address: null,
+      phonenumber: null,
     };
   }
 
   componentDidMount() {
     this.getBusinessInfo(1);
-    this.getBusinessAddress(1); 
+    this.getBusinessAddress(1);
   }
 
   getBusinessInfo(ID) {
@@ -51,13 +53,27 @@ class App extends React.Component {
     });
   }
 
-  getBusinessAddress() {
+  getBusinessAddress(ID) {
     $.ajax({
       type: 'GET',
       url: `/businesses/${ID}/business_map`,
       contentType: 'application/json',
       success: (data) => {
-  
+        const address = data[0].address;
+        this.setState({
+          address: address,
+        });
+        Geocode.setApiKey(Api.KEY);
+        Geocode.fromAddress('388 fulton st, san francisco, CA 94102').then(
+          response => {
+            const { lat, lng } = response.results[0].geometry.location;
+            console.log(lat, lng);
+
+          },
+          error => {
+            console.error(error);
+          }
+        );
       },
     });
   }
--- server/server.js
+++ server/server.js
@@ -16,9 +16,21 @@ app.get('/businesses/:business_id/business_info', (req, res) => {
   const queryStr = `SELECT * from business_info, more_business_info WHERE business_info.business_id = more_business_info.business_id AND business_info.business_id = ${businessId}`;
   connection.query(queryStr, (err, data) => {
     if (err) {
-      res.send(err);
+      res.status(404).send(err);
     } else {
-      res.send(data);
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get('/businesses/:business_id/business_map', (req, res) => {
+  const businessId = Number(req.params.business_id);
+  const queryStr = `SELECT * from map WHERE business_id = ${businessId}`;
+  connection.query(queryStr, (err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(data);
     }
   });
 });
