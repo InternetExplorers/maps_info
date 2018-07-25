ff4809091c619a64ddb59a8d8274527d11cdd446
alilthumuch <alilthumuch@gmail.com>
2018-07-25 14:00:50 -0700

    prepare google maps skeleton

---
+++ client/src/Map.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+   render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+
+export default GoogleApiWrapper({
+  apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+})(MapContainer)
