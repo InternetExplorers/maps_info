5beb5d5784f972774cb7411d5ba5eb7601159366
alilthumuch <alilthumuch@gmail.com>
2018-07-26 00:14:55 -0700

    Adding style to business hours and info

--- client/src/Businesshours.jsx
+++ client/src/Businesshours.jsx
@@ -3,26 +3,27 @@ import React from 'react';
 const businessHours = (props) => {
   return (
     <div>
-      <span className="Hours">
+      <span className="HoursHeading">
       Hours
       </span>
-      <br></br>
-      <b>Mon</b> {props.businessHours.mon}
-      <br />
-      <b>Tue</b> {props.businessHours.tue}
-      <br />
-      <b>Wed</b> {props.businessHours.wed}
-      <br />
-      <b>Thu</b> {props.businessHours.thu}
-      <br />
-      <b>Fri</b> {props.businessHours.fri}
-      <br />
-      <b>Sat</b> {props.businessHours.sat}
-      <br />
-      <b>Sun</b> {props.businessHours.sun}
-      <br />
+      <div className="Hours">
+        <b>Mon</b> {props.businessHours.mon}
+        <br />
+        <b>Tue</b> {props.businessHours.tue}
+        <br />
+        <b>Wed</b> {props.businessHours.wed}
+        <br />
+        <b>Thu</b> {props.businessHours.thu}
+        <br />
+        <b>Fri</b> {props.businessHours.fri}
+        <br />
+        <b>Sat</b> {props.businessHours.sat}
+        <br />
+        <b>Sun</b> {props.businessHours.sun}
+        <br />
+      </div>
     </div>
   );
 };
 
-export default businessHours
+export default businessHours;
--- client/src/Businessinfo.jsx
+++ client/src/Businessinfo.jsx
@@ -3,34 +3,38 @@ import React from 'react';
 const businessInfo = (props) => {
   return (
     <div>
-      <b>More business info</b><br></br>
-      <b>Take Reservations</b> {props.businessInfo.take_reservations}
-      <br />
-      <b>Delivery</b> {props.businessInfo.delivery}
-      <br />
-      <b>Take-out</b> {props.businessInfo.take_out}
-      <br />
-      <b>Accepts Credit Cards</b> {props.businessInfo.accepts_credit_cards}
-      <br />
-      <b>Accepts Google Pay</b> {props.businessInfo.accepts_google_pay}
-      <br />
-      <b>Bike Parking</b> {props.businessInfo.bike_parking}
-      <br/>
-      <b>Wheelchair Accessible</b> {props.businessInfo.wheelchair_accessible}
-      <br />
-      <b>Good for Kids</b> {props.businessInfo.good_for_kids}
-      <br />
-      <b>Good for Groups</b> {props.businessInfo.good_for_groups}
-      <br />
-      <b>Wi-Fi</b> {props.businessInfo.wi_fi}
-      <br />
-      <b>Has TV</b> {props.businessInfo.has_tv}
-      <br />
-      <b>Water Service</b> {props.businessInfo.waiter_service}
-      <br />
-      <b>Caters</b> {props.businessInfo.caters}
-      <br />
-      <b>Gender Neutral Restrooms</b> {props.businessInfo.gender_neutral_restrooms}
+      <span class="MoreBusinessInfoHeading">
+      More business info
+      </span>
+      <div class="MoreBusinessInfo">
+        Take Reservations <b>{props.businessInfo.take_reservations}</b>
+        <br />
+        Delivery <b>{props.businessInfo.delivery}</b>
+        <br />
+        Take-out <b>{props.businessInfo.take_out}</b>
+        <br />
+        Accepts Credit Cards <b>{props.businessInfo.accepts_credit_cards}</b>
+        <br />
+        Accepts Google Pay <b>{props.businessInfo.accepts_google_pay}</b>
+        <br />
+        Bike Parking <b>{props.businessInfo.bike_parking}</b>
+        <br/>
+        Wheelchair Accessible <b>{props.businessInfo.wheelchair_accessible}</b>
+        <br />
+        Good for Kids <b>{props.businessInfo.good_for_kids}</b>
+        <br />
+        Good for Groups <b>{props.businessInfo.good_for_groups}</b>
+        <br />
+        Wi-Fi <b>{props.businessInfo.wi_fi}</b>
+        <br />
+        Has TV <b>{props.businessInfo.has_tv}</b>
+        <br />
+        Water Service <b>{props.businessInfo.waiter_service}</b>
+        <br />
+        Caters <b>{props.businessInfo.caters}</b>
+        <br />
+        Gender Neutral Restrooms <b>{props.businessInfo.gender_neutral_restrooms}</b>
+    </div>
     </div>
   );
 };
