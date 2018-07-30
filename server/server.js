d9a629a352be05f2b60a62aeff0b9b80e28f6530
alilthumuch <alilthumuch@gmail.com>
2018-07-30 16:30:18 -0700

    adding cross origin access on server

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/businesses/:business_id/business_info', (req, res) => {
   const businessId = Number(req.params.business_id);
   const queryStr = `SELECT * FROM business_info, more_business_info WHERE business_info.business_id = more_business_info.business_id AND business_info.business_id = ${businessId}`;
